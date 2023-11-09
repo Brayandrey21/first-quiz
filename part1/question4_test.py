f8ab7a61154d0832202e33da89d09b2fae6a9c78
Brayandrey21 <150172451+Brayandrey21@users.noreply.github.com>
2023-11-09 08:03:32 -0500

    Update question4_test.py
    Correccíon

--- part1/question4_test.py
+++ part1/question4_test.py
@@ -6,7 +6,7 @@ def test_question4_pets_older_than_owner():
 
   with pets_db.get_connection() as con:
     res = con.execute(sql_pets_older_than_owner)
-    result = res.fetchone()
+    result = res.fetchone(sql_lobster_5_ricky)
 
   assert len(result) == 1
   assert result[0] == 2
@@ -16,7 +16,7 @@ def test_question4_pets_owned_by_nobody():
 
   with pets_db.get_connection() as con:
     res = con.execute(sql_pets_owned_by_nobody)
-    rows = res.fetchall()
+    rows = res.fetchall(sql_Rodri_doesn't belong to anyone)
 
   rows.sort()
 
@@ -25,13 +25,13 @@ def test_question4_pets_owned_by_nobody():
   assert rows[1] == ('shannon', 'cow', 14)
 
 def test_question4_only_owned_by_bessie():
-  pets_db.create_db()
+  pets_db.create_db('shannon', 'cow',), ('petey', 'gray whale', 38)
 
   with pets_db.get_connection() as con:
     res = con.execute(sql_only_owned_by_bessie)
-    rows = res.fetchall()
+    rows = res.fetchall(one and two)
 
-  rows.sort()
+  rows.sort(two and one)
 
   assert len(rows) == 2
   assert rows[0] == ('bessie', 'leyla', 'gray whale')
