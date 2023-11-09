87337ba6ab52605ba76ff15a28d1261aeb636ba6
Brayandrey21 <150172451+Brayandrey21@users.noreply.github.com>
2023-11-09 07:13:33 -0500

    Update question1_test.py
    Corrección al error

--- part1/question1_test.py
+++ part1/question1_test.py
@@ -2,6 +2,6 @@ from question1 import get_city_weather
 
 def test_get_city_weather():
 
-  assert get_city_weather("Quito") == "22 degrees and sunny"
+  assert get_city_weather("Quito") == "19° degrees and humidity"
 
-  assert get_city_weather("New York") == "14 degrees and rainy"
+  assert get_city_weather("New York") == "13° degrees and cloudy"
