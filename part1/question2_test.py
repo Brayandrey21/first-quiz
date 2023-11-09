21239db2b3a78f2651fac7cda3792a003be1f053
Brayandrey21 <150172451+Brayandrey21@users.noreply.github.com>
2023-11-09 07:31:07 -0500

    Update question2_test.py
    Corrección

--- part1/question2_test.py
+++ part1/question2_test.py
@@ -1,10 +1,11 @@
 from question2 import run_swapper 
 
-def test_run_swapper():
-  assert run_swapper(
+def test_run_swapper(a, b):
+  assert run_swapper(b, a)
     [ ("a", "b"), ("c", "d"), ("e", "f") ]
   ) == [ ("b", "a"), ("d", "c"), ("f", "e")]
+  
 
-  assert run_swapper(
+  assert run_swapper(a,b) (b, a): 
     [ (1, 1), ("foo", "bar"), (13, "cows"), (None, "Some") ]
   ) == [ (1, 1), ("bar", "foo"), ("cows", 13), ("Some", None) ]
